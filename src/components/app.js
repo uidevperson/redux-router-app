996393e536d10f20c510a1262952e19aa0393718
uidevperson <uidevthing@gmail.com>
2017-12-15 19:58:38 +0000

    update

--- src/components/app.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class App extends Component {
-  render() {
-    return (
-      <div>Redux router simple starter</div>
-    );
-  }
-}
---
+++ src/components/posts_index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'React';
+
+class PostsIndex extends Component {
+	render() {
+		return (
+			<div>PostsIndex</div>
+		);
+	}
+}
+
+export default PostsIndex;
