221650ae4ff01c1a89ac82ae1974f961834d1576
uidevperson <uidevthing@gmail.com>
2017-12-16 17:11:07 +0000

    render post to DOM

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'React';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
@@ -8,11 +9,29 @@ class PostsIndex extends Component {
 		this.props.fetchPosts();
 	}
 
+	renderPosts(){
+		return _.map(this.props.posts, (post) => {
+			return (
+				<li key={post.id} className ='list-group-item'>{post.title}</li>
+			);
+		});
+	}
+
 	render() {
+		// console.log({this.props.posts.map(prop => prop.id)});
 		return (
-			<div>PostsIndex</div>
+		<div>
+			<h3>Posts</h3>
+			<ul className="list-group">
+				{this.renderPosts()}
+			</ul>
+		</div>
 		);
 	}
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+function mapStateToProps(state) {
+	return { posts: state.posts};
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
