8d5869c93ffd9a6109757fce47a7eff639edb8c4
uidevperson <uidevthing@gmail.com>
2017-12-16 17:25:32 +0000

    add posts new router/component

---
+++ src/components/posts_new.js
@@ -0,0 +1,17 @@
+// import _ from 'lodash';
+import React, { Component } from 'React';
+// import { connect } from 'react-redux';
+// import { fetchPosts } from '../actions';
+
+class PostsNew extends Component {
+		render() {
+			return (
+			<div>
+				POSTSNEW
+			</div>
+			);
+		}
+	}
+
+
+export default PostsNew;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route} from 'react-router-dom';
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+
 import promise from 'redux-promise';
 //
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
@@ -13,7 +15,8 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
     	<div>
-		   <Route path='/' component={PostsIndex} /> 
+		   <Route path='/' component={ PostsIndex } /> 
+		   <Route path='/posts/new' component={ PostsNew } /> 
     	</div>
     </BrowserRouter>
   </Provider>
